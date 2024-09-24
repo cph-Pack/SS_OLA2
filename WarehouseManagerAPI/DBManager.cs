95e0e2fbb3c03bbfd102449ebd4656daa1e35c27
albiek1 <54135401+albiek1@users.noreply.github.com>
2024-09-24 14:38:41 +0200

    added frontend project + api

---
+++ WarehouseManagerAPI/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+
+namespace WarehouseManagerAPI
+{
+    public class DBManager
+    {
+        private const string connectionUri = "mogodb://localhost:27017";
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection
+    }
+}
